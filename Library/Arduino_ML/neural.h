b0b332db83ef0e7ec43ee7d49d8200a2fbd1c5f5
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-07-16 13:11:38 +0100

    Update neural.h

--- Library/Arduino_ML/neural.h
+++ Library/Arduino_ML/neural.h
@@ -1,15 +1,19 @@
 
 class Linear {
   private:
-    int in
-    int out
-
+    int in;
+    int out;
+    float** W;
+    float* b;
   public:
     Linear(int inp,int outp){
       in=inp;
       out=outp;
-      float W[inp][out] = {};
-      float b[1][out] = {};
+      W = new float*[inp];
+      for (int i = 0; i < inp; i++) {
+          W[i] = new float[out];
+      }
+      b = new float[out];
       for(int i=0;i<inp;i++){ //populate the linear layers with random float
         for(int j=0;j<out;j++){
           W[i][j]=randomFloat(-1,1);
@@ -28,18 +32,58 @@ class Linear {
 
 class neuralModel {
   private:
-    float MAX_LAYERS
-    int pointer
-    Linear *layers
+    int MAX_LAYERS;
+    int pointer;
+    Linear** layers;
   public:
     neuralModel(){
       MAX_LAYERS=5;
       pointer=0;
-      Linear *layers[MAX_LAYERS]; //create the array to have a set maximum
+      layers = new Linear*[MAX_LAYERS]; //create the array to have a set maximum
     }
     void addLayer(int in,int out){
-      layers[pointer]=Linear(in,out);
+      layers[pointer] = new Linear(in, out);
       pointer+=1;
+      if(pointer==MAX_LAYERS){ //if we add beyond default increase the total size
+        Linear** temp = new Linear*[MAX_LAYERS];
+        for (int i = 0; i < MAX_LAYERS; i++) {
+            temp[i] = layers[i];
+        }
+        delete[] layers;
+        MAX_LAYERS *= 2;
+        layers = new Linear*[MAX_LAYERS];
+        for (int i = 0; i < MAX_LAYERS / 2; i++) {
+            layers[i] = temp[i];
+        }
+        delete[] temp;
+        }
     }
     
+    float** dotProduct(float** A, int A_rows, int A_cols, float** B, int B_rows, int B_cols) {
+      if (A_cols != B_rows) {
+        Serial.println("Incompatible dimensions for dot product");
+        return nullptr;
+      }
+
+      float** result = new float*[A_rows]; 
+      for (int i = 0; i < A_rows; i++) {
+        result[i] = new float[B_cols];
+        for (int j = 0; j < B_cols; j++) {
+          result[i][j] = 0;
+          for (int k = 0; k < A_cols; k++) {
+            result[i][j] += A[i][k] * B[k][j];
+          }
+        }
+      }
+
+      return result; // reutn the resuting dot product 
+} 
+
+    float ** forward(float** inputs){
+      float** x=inputs;
+      for (int i=0;i<pointer;i++){
+        x = dotProduct(x,i);
+      }
+      
+    }
 }
